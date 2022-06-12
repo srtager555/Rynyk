a34dd99300dab0b545c660ac8c67ac0c941770e7
Carlos && Ponce <srtager555@gmail.com>
2022-06-12 16:04:01 -0600

    test response 4

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import discordApp from "../src/index";
+
 export default function app() {
+
+    useEffect(() => {
+        discordApp();
+    },[]);
 
     return ('XD');
 }
