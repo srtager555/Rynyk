f320e8f1f775f90045399d69e532a26898a3f9f5
Carlos && Ponce <srtager555@gmail.com>
2022-09-13 00:25:58 -0600

    I added the command "start-antispam" to the "function hall"

--- src/commands/slash--commands/index.js
+++ src/commands/slash--commands/index.js
@@ -1,16 +1,22 @@
 const { __slash_command__ping_latency__ } = require("./ping-latency.command");
+const {
+  __slash_command__start_antispam__,
+} = require("./start-antispam.command");
 
 // A simple function Hall
 function __interaction__slash_commands__(interaction) {
   const { commandName } = interaction;
 
-  const slashCommands = [__slash_command__ping_latency__];
+  const slashCommands = [
+    __slash_command__ping_latency__,
+    __slash_command__start_antispam__,
+  ];
 
   // here the code'll filter the correct command
   const whichCommandIs = slashCommands.filter((el) => el.name === commandName);
 
   // run the function of this command
-  whichCommandIs[0].functionToRun();
+  whichCommandIs[0].functionToRun({ interaction });
 }
 
 module.exports = { __interaction__slash_commands__ };
