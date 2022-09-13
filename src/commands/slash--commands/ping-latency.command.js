91b3cdb8531d7389eaecb98d4f9b8077af00004a
Carlos && Ponce <srtager555@gmail.com>
2022-09-12 23:08:07 -0600

    I upgraded the command with GlobalCommand

--- src/commands/slash--commands/ping-latency.command.js
+++ src/commands/slash--commands/ping-latency.command.js
@@ -1,4 +1,11 @@
-async function __slash_command__ping_latency__(interaction) {
+const { GlobalCommand } = require("../GlobalCommand");
+
+const __slash_command__ping_latency__ = new GlobalCommand({
+  name: "ping",
+  functionToRun: __slash_command__ping_latency__function,
+});
+
+async function __slash_command__ping_latency__function(interaction) {
   let time = 0;
   let timer = setInterval(() => time++, 1);
 
