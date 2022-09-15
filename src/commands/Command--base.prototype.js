4adc24034923e698ed39f69164801cee33feee52
Carlos && Ponce <srtager555@gmail.com>
2022-09-14 22:27:33 -0600

    I changed the name of the prototype "GlobalCommand" to "__prototype_command_base__"

---
+++ src/commands/Command--base.prototype.js
@@ -0,0 +1,6 @@
+function __prototype_command_base__({ name = "", functionToRun = Function }) {
+  this.name = name;
+  this.functionToRun = functionToRun;
+}
+
+module.exports = { __prototype_command_base__ };
--- src/commands/GlobalCommand.js
+++
@@ -1,6 +0,0 @@
-function GlobalCommand({ name = "", functionToRun = Function }) {
-  this.name = name;
-  this.functionToRun = functionToRun;
-}
-
-module.exports = { GlobalCommand };
